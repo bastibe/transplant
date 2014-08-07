76422cae074761b6d1d28c035fb9ac1d4ab6e77b
Bastian Bechtold <basti@bastibe.de>
2014-08-07 16:22:34 +0200

    implemented message protocol
    all messages are now sent as JSON objects.

--- transplant.m
+++ transplant.m
@@ -1,23 +1,64 @@
-%TRANSPLANT receives commands via 0MQ
-% TRANSPLANT(URL) connects to a 0MQ server at a given URL.
-%    The server can send two kinds of messages:
-%    - "die": Quit Matlab.
-%    - anything else: eval the message
+%TRANSPLANT is a Matlab server for remote code execution
+% TRANSPLANT(URL) connects to a 0MQ client at a given URL.
+%
+%    The client can send messages, which TRANSPLANT will answer.
+%    All messages are JSON-encoded strings. All message are structures
+%    with two keys: 'type' and 'content'.
+%
+%    TRANSPLANT implements the following message types:
+%    - 'eval': evaluates the content of the message.
+%    - 'die': closes the 0MQ session and quits Matlab.
+%
+%    TRANSPLANT implements the following responses:
+%    - 'ack': received message successfully.
+%    - 'error': there was an error while handling the message.
+
 function transplant(url)
+  addpath('jsonlab')
+
     messenger('open', url)
 
     while 1
-        msg = messenger('receive')
-        if (strcmp(msg, 'die'))
-           messenger('send', 'ack')
-           quit
-        else
-            try
-               eval(msg)
-            catch err
-               disp(err)
-            end
+
+        [msg_type, msg] = receive_msg();
+
+        switch msg_type
+            case 'die'
+                send_ack();
+                quit;
+            case 'eval'
+                try
+                    eval(msg);
+                    send_ack();
+                catch err
+                    send_error(err);
+                end
         end
-        messenger('send', 'ack');
     end
+end
+
+
+% Wait for and receive a message
+function [msg_type, msg_content] = receive_msg()
+    blob = messenger('receive');
+    data = loadjson(blob);
+    msg_type = data.type;
+    msg_content = data.content;
+end
+
+% Send a message
+function send_msg(msg_type, msg_content)
+    data.type = msg_type;
+    data.content = msg_content;
+    messenger('send', savejson('', data));
+end
+
+
+function send_ack()
+    send_msg('ack', '');
+end
+
+function send_error(err)
+    send_msg('error', struct('identifier', err.identifier, ...
+                             'message', err.message))
 end
--- transplant.py
+++ transplant.py
@@ -2,6 +2,24 @@ from subprocess import Popen
 import tempfile
 import zmq
 
+
+"""Transplant is a Python client for remote code execution
+
+It can start and connect Matlab servers and send them messages. All
+messages are JSON-encoded strings. All messages are dictionaries with
+two keys: 'type' and 'content'.
+
+These message types are implemented:
+- 'eval': the server evaluates the content of the message.
+- 'die': the server closes its 0MQ session and quits.
+
+These response types are implemented:
+- 'ack': the server received the message successfully.
+- 'error': there was an error while handling the message.
+
+"""
+
+
 class Matlab:
     """An instance of Matlab, running in its own process."""
 
@@ -14,13 +32,21 @@ class Matlab:
         self.process = Popen(['matlab',
                               '-r', "transplant {}".format('ipc://' + self.ipcfile.name)])
 
-    def eval(self, msg):
+    def eval(self, msg_content):
         """Send some code to Matlab to execute."""
-        self.socket.send_string(msg)
-        return self.socket.recv()
+        result = self.send_message('eval', msg_content)
+        if result['type'] == 'error':
+            raise RuntimeError(result['content'])
+        elif result['content']:
+            return result['content']
 
     def __del__(self):
         """Close the connection, and kill the process."""
-        self.socket.send_string('die')
-        self.socket.recv()
+        self.send_message('die')
         self.process.terminate()
+
+    def send_message(self, msg_type, msg_content=''):
+        """Send a message and return the response"""
+        self.socket.send_json({'type':msg_type,
+                               'content':msg_content})
+        return self.socket.recv_json()
