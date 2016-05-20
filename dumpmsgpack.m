0be6282e5d64ba5c02ab1b0b39f66cc996a12621
Bastian Bechtold <basti@bastibe.de>
2016-05-20 15:29:26 +0200

    add messagepack parser/writer

---
+++ dumpmsgpack.m
@@ -0,0 +1,201 @@
+%DUMPMSGPACK dumps Matlab data structures as a msgpack data
+% DUMPMSGPACK(DATA)
+%    recursively walks through DATA and creates a msgpack byte buffer from it.
+%    - strings are converted to strings
+%    - scalars are converted to numbers
+%    - logicals are converted to `true` and `false`
+%    - arrays are converted to arrays of numbers
+%    - matrices are converted to arrays of arrays of numbers
+%    - empty matrices are converted to nil
+%    - cell arrays are converted to arrays
+%    - cell matrices are converted to arrays of arrays
+%    - struct arrays are converted to arrays of maps
+%    - structs and container.Maps are converted to maps
+%    - function handles and matlab objects will raise an error.
+%
+%    There is no way of encoding bins or exts
+
+% (c) 2016 Bastian Bechtold
+% This code is licensed under the BSD 3-clause license
+
+function msgpack = dumpmsgpack(data)
+    msgpack = dump(data);
+end
+
+function msgpack = dump(data)
+    % convert numeric matrices to cell matrices since msgpack doesn't know matrices
+    if (isnumeric(data) || islogical(data)) && ~isscalar(data) && ~isempty(data)
+        data = num2cell(data);
+    end
+    % convert character matrices to cell of strings or cell matrices
+    if ischar(data) && ~isvector(data) && ndims(data) == 2
+        data = cellstr(data);
+    elseif ischar(data) && ~isvector(data)
+        data = num2cell(data);
+    end
+    % convert struct arrays to cell of structs
+    if isstruct(data) && ~isscalar(data)
+        data = num2cell(data);
+    end
+    % standardize on always using maps instead of structs
+    if isstruct(data)
+        data = containers.Map(fieldnames(data), struct2cell(data));
+    end
+
+    if isnumeric(data) && isempty(data)
+        msgpack = uint8(192); % encode nil
+    elseif islogical(data)
+        if data
+            msgpack = uint8(195); % encode true
+        else
+            msgpack = uint8(194); % encode false
+        end
+    elseif isinteger(data)
+        msgpack = dumpinteger(data);
+    elseif isnumeric(data)
+        msgpack = dumpfloat(data);
+    elseif ischar(data)
+        msgpack = dumpstring(data);
+    elseif iscell(data)
+        msgpack = dumpcell(data);
+    elseif isa(data, 'containers.Map')
+        msgpack = dumpmap(data);
+    else
+        error('transplant:dumpmsgpack:unknowntype', ...
+              ['Unknown type "' class(data) '"']);
+    end
+end
+
+function bytes = scalar2bytes(value)
+    % reverse byte order to convert from little endian to big endian
+    bytes = typecast(swapbytes(value), 'uint8');
+end
+
+function msgpack = dumpinteger(value)
+    b11110000 = 240;
+
+    % if the values are small enough, encode as fixnum:
+    if value >= 0 && value < 128
+        % first bit is 0, last 7 bits are value
+        msgpack = uint8(value);
+        return
+    elseif value < 0 && value > -32
+        % first three bits are 111, last 5 bytes are value
+        msgpack = uint8(bitor(-value, b11100000));
+        return
+    end
+
+    % otherwise, encode by type:
+    switch class(value)
+        case 'uint8' % encode as uint8
+            msgpack = uint8([204, value]);
+        case 'uint16' % encode as uint16
+            msgpack = uint8([205, scalar2bytes(value)]);
+        case 'uint32' % encode as uint32
+            msgpack = uint8([206, scalar2bytes(value)]);
+        case 'uint64' % encode as uint64
+            msgpack = uint8([207, scalar2bytes(value)]);
+        case 'int8' % encode as int8
+            msgpack = uint8([208, scalar2bytes(value)]);
+        case 'int16' % encode as int16
+            msgpack = uint8([209, scalar2bytes(value)]);
+        case 'int32' % encode as int32
+            msgpack = uint8([210, scalar2bytes(value)]);
+        case 'int64' % encode as int64
+            msgpack = uint8([211, scalar2bytes(value)]);
+        otherwise
+            error('transplant:dumpmsgpack:unknowninteger', ...
+                  ['Unknown integer type "' class(value) '"']);
+    end
+end
+
+function msgpack = dumpfloat(value)
+    % do double first, as it is more common in Matlab
+    if isa(value, 'double') % encode as float64
+        msgpack = uint8([203, scalar2bytes(value)]);
+    elseif isa(value, 'single') % encode as float32
+        msgpack = uint8([202, scalar2bytes(value)]);
+    else
+        error('transplant:dumpmsgpack:unknownfloat', ...
+              ['Unknown float type "' class(value) '"']);
+    end
+end
+
+function msgpack = dumpstring(value)
+    b10100000 = 160;
+
+    encoded = unicode2native(value, 'utf-8');
+    len = length(encoded);
+
+    if len < 32 % encode as fixint:
+        % first three bits are 101, last 5 are length:
+        msgpack = [uint8(bitor(len, b10100000)), encoded];
+    elseif len < 256 % encode as str8
+        msgpack = [uint8([217, len]), encoded];
+    elseif len < 2^16 % encode as str16
+        msgpack = [uint8(218), scalar2bytes(uint16(len)), encoded];
+    elseif len < 2^32 % encode as str32
+        msgpack = [uint8(219), scalar2bytes(uint32(len)), encoded];
+    else
+        error('transplant:dumpmsgpack:stringtoolong', ...
+              sprintf('String is too long (%d bytes)', len));
+    end
+end
+
+function msgpack = dumpcell(value)
+    b10010000 = 144;
+
+    % Msgpack can only work with 1D-arrays. Thus, Convert a
+    % multidimensional AxBxC array into a cell-of-cell-of-cell, so
+    % that indexing value{a, b, c} becomes value{a}{b}{c}.
+    if length(value) ~= prod(size(value))
+        for n=ndims(value):-1:2
+            value = cellfun(@squeeze, num2cell(value, n), ...
+                            'uniformoutput', false);
+        end
+    end
+
+    % write header
+    len = length(value);
+    if len < 16 % encode as fixarray
+        % first four bits are 1001, last 4 are length
+        msgpack = [uint8(bitor(len, b10010000))];
+    elseif len < 2^16 % encode as array16
+        msgpack = [uint8(220), scalar2bytes(uint16(len))];
+    elseif len < 2^32 % encode as array32
+        msgpack = [uint8(221), scalar2bytes(uint32(len))];
+    else
+        error('transplant:dumpmsgpack:arraytoolong', ...
+              sprintf('Array is too long (%d elements)', len));
+    end
+
+    % write values
+    for n=1:len
+        msgpack = [msgpack, dump(value{n})];
+    end
+end
+
+function msgpack = dumpmap(value)
+    b10000000 = 128;
+
+    % write header
+    len = length(value);
+    if len < 16 % encode as fixmap
+        % first four bits are 1000, last 4 are length
+        msgpack = [uint8(bitor(len, b10000000))];
+    elseif len < 2^16 % encode as map16
+        msgpack = [uint8(222), scalar2bytes(uint16(len))];
+    elseif len < 2^32 % encode as map32
+        msgpack = [uint8(223), scalar2bytes(uint32(len))];
+    else
+        error('transplant:dumpmsgpack:maptoolong', ...
+              sprintf('Map is too long (%d elements)', len));
+    end
+
+    % write key-value pairs
+    keys = value.keys();
+    values = value.values();
+    for n=1:len
+        msgpack = [msgpack, dump(keys{n}), dump(values{n})];
+    end
+end
---
+++ parsemsgpack.m
@@ -0,0 +1,192 @@
+%PARSEMSGPACK parses a msgpack byte buffer into Matlab data structures
+% PARSEMSGPACK(BYTES)
+%    reads BYTES as msgpack data, and creates Matlab data structures
+%    from it.
+%    - strings are converted to strings
+%    - numbers are converted to appropriate numeric values
+%    - true, false are converted to logical 1, 0
+%    - nil is converted to []
+%    - arrays are converted to cell arrays
+%    - maps are converted to containers.Map
+
+% (c) 2016 Bastian Bechtold
+% This code is licensed under the BSD 3-clause license
+function obj = parsemsgpack(bytes)
+    [obj, ~] = parse(uint8(bytes(:)), 1);
+end
+
+function [obj, idx] = parse(bytes, idx)
+    % masks:
+    b10000000 = 128;
+    b01111111 = 127;
+    b11000000 = 192;
+    b00111111 = 63;
+    b11100000 = 224;
+    b00011111 = 31;
+    b11110000 = 240;
+    b00001111 = 15;
+    % values:
+    b00000000 = 0;
+    b10010000 = 144;
+    b10100000 = 160;
+
+    currentbyte = bytes(idx);
+
+    if bitand(b10000000, currentbyte) == b00000000
+        % decode positive fixint
+        obj = bitand(b01111111, currentbyte);
+        idx = idx + 1;
+        return
+    elseif bitand(b11100000, currentbyte) == b11100000
+        % decode negative fixint
+        obj = -bitand(b00011111, currentbyte);
+        idx = idx + 1;
+        return
+    elseif bitand(b11110000, currentbyte) == b10000000
+        % decode fixmap
+        len = double(bitand(b00001111, currentbyte));
+        [obj, idx] = parsemap(len, bytes, idx+1);
+        return
+    elseif bitand(b11110000, currentbyte) == b10010000
+        % decode fixarray
+        len = double(bitand(b00001111, currentbyte));
+        [obj, idx] = parsearray(len, bytes, idx+1);
+        return
+    elseif bitand(b11100000, currentbyte) == b10100000
+        % decode fixstr
+        len = double(bitand(b00011111, currentbyte));
+        [obj, idx] = parsestring(len, bytes, idx + 1);
+        return
+    end
+
+    switch currentbyte
+        case 192 % nil
+            obj = [];
+            idx = idx+1;
+      % case 193 % unused
+        case 194 % false
+            obj = false;
+            idx = idx+1;
+        case 195 % true
+            obj = true;
+            idx = idx+1;
+        case 196 % bin8
+            len = double(bytes(idx+1));
+            [obj, idx] = parsebytes(len, bytes, idx+2);
+        case 197 % bin16
+            len = double(bytes2scalar(bytes(idx+1:idx+2), 'uint16'));
+            [obj, idx] = parsebytes(len, bytes, idx+3);
+        case 198 % bin32
+            len = double(bytes2scalar(bytes(idx+1:idx+4), 'uint32'));
+            [obj, idx] = parsebytes(len, bytes, idx+5);
+        case 199 % ext8
+            len = double(bytes(idx+1));
+            [obj, idx] = parseext(len, bytes, idx+1);
+        case 200 % ext16
+            len = double(bytes2scalar(bytes(idx+1:idx+2), 'uint16'));
+            [obj, idx] = parseext(len, bytes, idx+3);
+        case 201 % ext32
+            len = double(bytes2scalar(bytes(idx+1:idx+4), 'uint32'));
+            [obj, idx] = parseext(len, bytes, idx+5);
+        case 202 % float32
+            obj = bytes2scalar(bytes(idx+1:idx+4), 'single');
+            idx = idx+5;
+        case 203 % float64
+            obj = bytes2scalar(bytes(idx+1:idx+8), 'double');
+            idx = idx+9;
+        case 204 % uint8
+            obj = bytes(idx+1);
+            idx = idx+2;
+        case 205 % uint16
+            obj = bytes2scalar(bytes(idx+1:idx+2), 'uint16');
+            idx = idx+3;
+        case 206 % uint32
+            obj = bytes2scalar(bytes(idx+1:idx+4), 'uint32');
+            idx = idx+5;
+        case 207 % uint64
+            obj = bytes2scalar(bytes(idx+1:idx+8), 'uint64');
+            idx = idx+9;
+        case 208 % int8
+            obj = bytes2scalar(bytes(idx+1), 'int8');
+            idx = idx+2;
+        case 208 % int16
+            obj = bytes2scalar(bytes(idx+1:idx+2), 'int16');
+            idx = idx+3;
+        case 210 % int32
+            obj = bytes2scalar(bytes(idx+1:idx+4), 'int32');
+            idx = idx+5;
+        case 211 % int64
+            obj = bytes2scalar(bytes(idx+1:idx+8), 'int64');
+            idx = idx+9;
+        case 212 % fixext1
+            [obj, idx] = parseext(1, bytes, idx+1);
+        case 213 % fixext2
+            [obj, idx] = parseext(2, bytes, idx+1);
+        case 214 % fixext4
+            [obj, idx] = parseext(4, bytes, idx+1);
+        case 215 % fixext8
+            [obj, idx] = parseext(8, bytes, idx+1);
+        case 216 % fixext16
+            [obj, idx] = parseext(16, bytes, idx+1);
+        case 217 % str8
+            len = double(bytes(idx+1));
+            [obj, idx] = parsestring(len, bytes, idx+2);
+        case 218 % str16
+            len = double(bytes2scalar(bytes(idx+1:idx+2), 'uint16'));
+            [obj, idx] = parsestring(len, bytes, idx+3);
+        case 219 % str32
+            len = double(bytes2scalar(bytes(idx+1:idx+4), 'uint32'));
+            [obj, idx] = parsestring(len, bytes, idx+5);
+        case 220 % array16
+            len = double(bytes2scalar(bytes(idx+1:idx+2), 'uint16'));
+            [obj, idx] = parsearray(len, bytes, idx+3);
+        case 221 % array32
+            len = double(bytes2scalar(bytes(idx+1:idx+4), 'uint32'));
+            [obj, idx] = parsearray(len, bytes, idx+5);
+        case 222 % map16
+            len = double(bytes2scalar(bytes(idx+1:idx+2), 'uint16'));
+            [obj, idx] = parsemap(len, bytes, idx+3);
+        case 223 % map32
+            len = double(bytes2scalar(bytes(idx+1:idx+4), 'uint32'));
+            [obj, idx] = parsemap(len, bytes, idx+5);
+        otherwise
+            error('transplant:parsemsgpack:unknowntype', ...
+                  ['Unknown type "' dec2bin(currentbyte) '"']);
+    end
+end
+
+function value = bytes2scalar(bytes, type)
+    % reverse byte order to convert from little-endian to big-endian
+    value = typecast(bytes(end:-1:1), type);
+end
+
+function [str, idx] = parsestring(len, bytes, idx)
+    str = native2unicode(bytes(idx:idx+len-1)', 'utf-8');
+    idx = idx + len;
+end
+
+function [out, idx] = parsebytes(len, bytes, idx)
+    out = bytes(idx:idx+len-1);
+    idx = idx + len;
+end
+
+function [out, idx] = parseext(len, bytes, idx)
+    obj.type = bytes(idx);
+    obj.data = bytes(idx+1:idx+len);
+    idx = idx+len+1;
+end
+
+function [out, idx] = parsearray(len, bytes, idx)
+    out = cell(1, len);
+    for n=1:len
+        [out{n}, idx] = parse(bytes, idx);
+    end
+end
+
+function [out, idx] = parsemap(len, bytes, idx)
+    out = containers.Map();
+    for n=1:len
+        [key, idx] = parse(bytes, idx);
+        [out(key), idx] = parse(bytes, idx);
+    end
+end
