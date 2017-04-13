526059cf21902f6c074db376329d4d7fa4688ccf
Bastian Bechtold <bastian.bechtold@jade-hs.de>
2017-04-13 09:24:08 +0200

    makes transplant installable as package

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Transplant',
+      version='0.7',
+      description='Call Matlab from Python (requires Matlab)',
+      author='Bastian Bechtold',
+      author_email='basti@bastibe.de',
+      url='https://github.com/bastibe/transplant',
+      packages=['transplant'],
+      package_data={'transplant': ['parsemsgpack.m', 'dumpmsgpack.m',
+                                   'parsejson.m', 'dumpjson.m',
+                                   'base64decode.m', 'base64encode.m',
+                                   'transplant_remote.m', 'ZMQ.m',
+                                   'transplantzmq.h']},
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: MacOS',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 3'],
+      license='BSD 3-clause License',
+      requires=['numpy', 'zmq', 'msgpack']
+     )
